97961004f72c066f0cf2df9748d51626561ac4c0
Dilendra Singh <105235344+dilendra-thug@users.noreply.github.com>
2025-11-01 16:35:40 +0545

    Update main.py and requirements.txt for latest LangChain setup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import traceback
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains import ConversationalRetrievalChain
